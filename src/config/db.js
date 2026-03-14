79c619888fe2c87bbab55020589af09708e5297b
Maro Egweni <youremail@example.com>
2026-03-14 19:22:27 -0400

    fix: add SSL for Neon PostgreSQL

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,10 @@
 import pg from 'pg';
 
-const {Pool} = pg;
+const { Pool } = pg;
 
 export const pool = new Pool({
-   connectionString:process.env.DATABASE_URL
-})
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
